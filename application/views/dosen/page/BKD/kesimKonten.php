6ec59e2c6f256d3c791d9f933155060429f224f8
muhammadsiraf <derekzify@gmail.com>
2019-05-22 10:21:38 +0700

    fix few pages minor fix only

--- application/views/dosen/page/BKD/kesimKonten.php
+++ application/views/dosen/page/BKD/kesimKonten.php
@@ -1,1 +1,112 @@
 <h1>kesimpulan BKD</h1>
+<?php $dosen=$this->session->userdata('dosen')?>
+<div class="container">
+ <div class="row">
+        <div class="col-lg">
+        <div class="card">
+        <h5 class="card-header"></h5>
+        <div class="card-body">
+          <table class="table">
+      <thead class="thead-bordered">
+      <tr>
+        <th scope="col">Nama: </th>
+        <th scope="col"><?php echo $dosen->nama?></th>
+      </tr>
+      <tr>
+        <th scope="col">Status BKD:</th>
+        <th scope="col">
+          <select class="form-control form-control-sm">
+            <option>Dosen Tetap</option>
+          </select>
+        </th>
+
+      </tr>
+      <tr>
+        <th scope="col">Tahun Akademik:</th>
+        <th scope="col">
+          <select class="form-control form-control-sm">
+            <option>2018/2019 || Genap</option>
+          </select>
+        </th>
+
+      </tr>
+      </thead>
+      </table>
+        </div>
+        </div>
+        </div>
+    </div>
+</div>
+ <br>
+    <div class="row">
+        <div class="col-lg">
+        <div class="card">
+        <h5 class="card-header"></h5>
+        <div class="card-body">
+        <table class="table">
+        <thead class="thead-dark">
+          <tr>
+            <th rowspan="2" scope="col">No</th>
+            <th rowspan="2" scope="col">Keterangan</th>
+            <th rowspan="2" scope="col">Syarat</th>
+            <th rowspan="2" scope="col">Kinerja (SKS)</th>
+            <th rowspan="2" scope="col">Kesimpulan</th>
+
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Pendidikan</td>
+            <td>Tidak Boleh Kosong</td>
+            <td>11.00</td>
+            <td>M</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Penelitian</td>
+            <td>Tidak Boleh Kosong</td>
+            <td>2.00</td>
+            <td>M</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Pengabdian</td>
+            <td>Tidak Boleh Kosong</td>
+            <td>1.00</td>
+            <td>M</td>
+          </tr>
+          <tr>
+            <th scope="row">4</th>
+            <td>Pendidikan + Penelitian</td>
+            <td>Min. 9 SKS></td>
+            <td>13</td>
+            <td>M</td>
+          </tr>
+          <tr>
+            <th scope="row">5</th>
+            <td>Pengabdian + Penunjang</td>
+            <td>Min. 3 SKS</td>
+            <td>3</td>
+            <td>M</td>
+          </tr>
+          <tr>
+            <th scope="row">6</th>
+            <td>Total Kinerja </td>
+            <td>Min 12 SKS Max 16 SKS</td>
+            <td>16</td>
+            <td>M</td>
+          </tr>
+        </tbody>
+          <tr>
+            <th scope="row">KESIMPULAN</th>
+            <td colspan="3" >Memenuhi Syarat</td>
+          </tr>
+      </table>
+        </div>
+        </div>
+        </div>
+    </div>
+</div>
+ 
--- application/views/dosen/page/BKD/pengabKonten.php
+++ application/views/dosen/page/BKD/pengabKonten.php
@@ -1,1 +1,74 @@
 <h1>Pengabdian BKD</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-lg">  
+            <div class="card">
+                <h5 class="card-header"></h5>
+                <div class="card-body">
+                <table class="table">
+        <thead class="thead-dark">
+          <tr>
+            <th rowspan="2" scope="col">No</th>
+            <th rowspan="2" scope="col">Uraian Kegiatan</th>
+            <th colspan="3" scope="col">Target</th>
+            <th colspan="3" scope="col">Realisasi</th>
+            <th rowspan="2" scope="col">SKS BKD</th>
+            <th rowspan="2" scope="col">Rekomendasi</th>
+            <th rowspan="2" scope="col">Dokumen</th>
+          </tr>
+          <tr>
+            <th scope="col">Jumlah</th>
+            <th scope="col">Satuan</th>
+            <th scope="col">Kualitas</th>
+            <th scope="col">Jumlah</th>
+            <th scope="col">Satuan</th>
+            <th scope="col">Kualitas</th>
+
+
+            <!-- <th rowspan="2" scope="col">***</th> -->
+
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Menduduki Jabatan Pimpinan di Perguruan Tinggi</td>
+            <td >7</td>
+            <td>sks</td>
+            <td>100</td>
+            <td>10</td>
+            <td>sks</td>
+            <td>100</td>
+            <td><input name="jumlahRealisasi" type="text" class="form-control" id="jumlahRealisasi" aria-describedby="" placeholder="Jumlah"></td>
+            <td><input name="kualitasRealisasi" type="text" class="form-control" id="kualitasRealisasi" aria-describedby="" placeholder="Kualitas "></td>
+            <td><button type="button" class="btn btn-warning">Lihat</button></td>
+          </tr>
+
+          <tr>
+            <th scope="row">2</th>
+            <td>Anggota Redaksi Jurnal Ber-ISSN</td>
+            <td >1</td>
+            <td>kegiatan</td>
+            <td>100</td>
+            <td>0</td>
+            <td>kegiatan</td>
+            <td>0</td>
+            <td><input name="jumlahRealisasi" type="text" class="form-control" id="jumlahRealisasi" aria-describedby="" placeholder="Jumlah"></td>
+            <td><input name="kualitasRealisasi" type="text" class="form-control" id="kualitasRealisasi" aria-describedby="" placeholder="Kualitas "></td>
+            <td><button type="button" class="btn btn-danger" disabled>Kosong</button></td>
+          </tr>
+         
+          <tr>
+            <th colspan="5" scope="row"></th>
+            <td><button type="button" class="btn btn-success">Simpan</button></td>
+          </tr>
+        </tbody>
+
+      </table>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
