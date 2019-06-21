29a85f6036b8f9f04162d35376e43e96b3e82eb4
muhammadsiraf <derekzify@gmail.com>
2019-06-21 10:40:51 +0700

    add lines of code in view on simulasi serdos

--- application/views/dosen/page/serdos/simulasiKonten.php
+++ application/views/dosen/page/serdos/simulasiKonten.php
@@ -1,1 +1,173 @@
 <h1>Simulasi Pengajuan Serdos</h1>
+<div class="container">
+<div class="row">
+<div class="col-lg">
+<div class="card">
+<h5 class="card-header">Konversi NKP</h5>
+<div class="card-body">
+<form action="">
+<label for="" >Golongan/Pangkat</label>
+<input type="text" name="Golongan" id="" class="form-control">
+<br>
+<button type="submit" class="btn btn-primary">Hitung</button>
+</form>
+</div>
+</div>
+</div>
+</div>
+<br>
+<div class="row">
+<div class="col-lg">
+<div class="card">
+<h5 class="card-header">Konversi NAP</h5>
+<div class="card-body">
+<form action="">
+<label for="" >Jabatan Akademik</label>
+<input type="text" name="Jabatan" id="" class="form-control">
+<br>
+<button type="submit" class="btn btn-primary">Hitung</button>
+</form>
+</div>
+</div>
+</div>
+</div>
+<br>
+<div class="row">
+<div class="col-lg">
+<div class="card">
+<h5 class="card-header">Konversi TKBI</h5>
+<div class="card-body">
+
+<h5>Hitung Skor Toelf PBT:</h5>
+<form action="">
+<label for="" >Nilai Toelf</label>
+<input type="text" name="TOELFPBT" id="" class="form-control">
+<br>
+<button type="submit" class="btn btn-primary">Hitung</button>
+</form>
+<br>
+<h5>Hitung Skor Toelf CBT:</h5>
+<form action="">
+<label for="" >Nilai Toelf</label>
+<input type="text" name="TOELFCBT" id="" class="form-control">
+<br>
+<button type="submit" class="btn btn-primary">Hitung</button>
+</form>
+<br>
+<h5>Hitung Skor Toelf iBT:</h5>
+<form action="">
+<label for="" >Nilai Toelf</label>
+<input type="text" name="TOELFIBT" id="" class="form-control">
+<br>
+<button type="submit" class="btn btn-primary">Hitung</button>
+</form>
+<br>
+<h5>Hitung Skor IELTS:</h5>
+<form action="">
+<label for="" >Nilai IELTS</label>
+<input type="text" name="IELTS" id="" class="form-control">
+<br>
+<button type="submit" class="btn btn-primary">Hitung</button>
+</form>
+<br>
+<h5>Hitung Skor TOEP:</h5>
+<form action="">
+<label for="" >Nilai TOEP</label>
+<input type="text" name="TOEP" id="" class="form-control">
+<br>
+<button type="submit" class="btn btn-primary">Hitung</button>
+</form>
+<br>
+</div>
+</div>
+</div>
+</div>
+
+<br>
+<div class="row">
+<div class="col-lg">
+<div class="card">
+<h5 class="card-header">Konversi TKDA</h5>
+<div class="card-body">
+<form action="">
+<label for="" >Nilai TKDA</label>
+<input type="text" name="TKDA" id="" class="form-control">
+<br>
+<button type="submit" class="btn btn-primary">Hitung</button>
+</form>
+</div>
+</div>
+</div>
+</div>
+<br>
+<div class="row">
+<div class="col-lg">
+<div class="card">
+<h5 class="card-header">Kalkulator Rekomendasi Skor Persepsional Min</h5>
+<div class="card-body">
+<form action="">
+<label for="" >Jabatan Akademik</label>
+<input type="text" name="Jabatan" id="" class="form-control">
+<br>
+<label for="" >Pendidikan Tertinggi</label>
+<input type="text" name="Pendidikan" id="" class="form-control">
+<br>
+<label for="" >Golongan/Pangkat</label>
+<input type="text" name="Golongan" id="" class="form-control">
+<br>
+<label for="" >TOEP</label>
+<input type="text" name="TOEP" id="" class="form-control">
+<br>
+<label for="" >TKDA</label>
+<input type="text" name="TKDA" id="" class="form-control">
+<br>
+<button type="submit" class="btn btn-primary">Hitung</button>
+</form>
+</div>
+</div>
+</div>
+</div>
+
+<br>
+<div class="row">
+<div class="col-lg">
+<div class="card">
+<h5 class="card-header">Simulasi Penilaian Persepsional</h5>
+<div class="card-body">
+
+</div>
+</div>
+</div>
+</div>
+
+<br>
+<div class="row">
+<div class="col-lg">
+<div class="card">
+<h5 class="card-header">Simulasi Nilai Gabungan</h5>
+<div class="card-body">
+<form action="">
+<label for="" >Skor NAP</label>
+<input type="text" name="NAP" id="" class="form-control">
+<br>
+<label for="" >Skor NKP</label>
+<input type="text" name="NKP" id="" class="form-control">
+<br>
+<label for="" >Skor NPS</label>
+<input type="text" name="NPS" id="" class="form-control">
+<br>
+<label for="" >Skor NBI</label>
+<input type="text" name="NBI" id="" class="form-control">
+<br>
+<label for="" >Skor NPA</label>
+<input type="text" name="NPA" id="" class="form-control">
+<br>
+<button type="submit" class="btn btn-primary">Hitung</button>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
+<br>
+
