492c271164f98731d5838cad576cc0aebca2b8d9
muhammadsiraf <derekzify@gmail.com>
2019-06-21 09:54:42 +0700

    add view on simulasi

--- application/views/dosen/page/jabatan/penilaianKonten.php
+++ application/views/dosen/page/jabatan/penilaianKonten.php
@@ -1,1 +1,49 @@
 <h1>Simulasi Penilaian</h1>
+
+<div class="container">
+<div class="row">
+<div class="col-lg">
+
+<div class="card">
+<h3 class="card-header">Status</h3>
+<div class="card-body">
+<h2>Jabatan Anda Sekarang: </h2>
+<h2>Golongan : </h2>
+<h3>Kredit Kumulatif Anda : </h3>
+<h3>Kredit Kumulatif Pendidikan : </h3>
+<h3>Kredit Kumulatif Penelitian  : </h3>
+<h3>Kredit Kumulatif Pengabdian : </h3>
+<h3>Kredit Kumulatif Penunjang  : </h3>
+</div>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="col-lg">
+<div class="card">
+<div class="card-body">
+<h3>Simulasikan perhitungan kredit kenaikan jabatan</h3> 
+<form action="">
+<div class="form-group">
+<h3><label for="label">Jabatan Yang Dituju: </label></h3>
+<select class="custom-select my-1 mr-sm-2" nama="jabatan" id="inlineFormCustomSelectPref">
+    <option selected>Pilih...</option>
+    <option value="1">Asisten Ahli Gol III/b</option>
+    <option value="2">Lektor Gol III/c</option>
+    <option value="3">Lektor Gol III/d< /option>
+    <option value="4">Lektor Kepala Gol IV/a</option>
+    <option value="5">Lektor Kepala Gol IV/b</option>
+    <option value="6">Lektor Kepala Gol IV/c</option>
+    <option value="6">Guru Besar Gol IV/d</option>
+    <option value="6">Guru Besar  Gol IV/e</option>
+</select>
+<button type="submit" class="btn btn-primary">Simulasikan</button>
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<br>
